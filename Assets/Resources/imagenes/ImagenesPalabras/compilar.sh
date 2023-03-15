34b66beb534640f5b2c105d833ca8a1e106d6949
PCRGB <gdvestudios@gmail.com>
2023-03-15 10:43:07 -0300

    bastaDePngs

--- Assets/Resources/imagenes/ImagenesPalabras/compilar.sh
+++ Assets/Resources/imagenes/ImagenesPalabras/compilar.sh
@@ -1,8 +1,9 @@
-rm -f ./output/$1.png
+#rm -f ./output/$1.png
 for img in *.png
 do 
-   #convert wumpus_base.png $img -composite ./output/$img
-    convert *.jpg -resize 128x128 -gravity center -extent 128x128 ./output/*.png
+    convert *.jpg -resize 128x128 -gravity center -extent 128x128 *.png
+    read -p "Todas las imagenes convertidas " -n1 -s
+    exit 0
 done
 #convert -append ./output/*$1*.png ./output/$1.png
 #rm -f ./output/*.png
