4a06455782584119eca244e80a18744cffd4dc8c
PCRGB <gdvestudios@gmail.com>
2023-03-27 11:03:30 -0300

    musicas

---
+++ Assets/Scripts/TextToSpeech.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class TextToSpeech : MonoBehaviour
+{
+    public TextToSpeech Hablar;
+    public string silaba;
+
+}
